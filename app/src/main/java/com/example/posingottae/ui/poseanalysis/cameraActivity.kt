d1b03865c1b650c9c0e67b2a0fa2fae6b84ba030
eonseonpark <pocva6243@gmail.com>
2023-12-05 01:40:33 +0900

    캡쳐된거 로컬 푸시알림 구현하기위해 uri 넘기는 코드 추가

--- app/src/main/java/com/example/posingottae/ui/poseanalysis/cameraActivity.kt
+++ app/src/main/java/com/example/posingottae/ui/poseanalysis/cameraActivity.kt
@@ -1,8 +1,8 @@
-package com.example.posingottae
+package com.example.posingottae.ui.poseanalysis
 
+import LocalNotificationManager
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -12,6 +12,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -22,20 +23,27 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.posingottae.databinding.ActivityCameraBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseDetection
 import com.google.mlkit.vision.pose.PoseDetector
+import com.google.mlkit.vision.pose.PoseLandmark
 import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
+import org.json.JSONException
+import org.json.JSONObject
+import java.lang.reflect.Method
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-typealias LumaListener = (luma: Double) -> Unit
-
-class cameraActivity : AppCompatActivity() {
+class CameraActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityCameraBinding
 
     private var imageCapture: ImageCapture? = null
@@ -55,44 +63,44 @@ class cameraActivity : AppCompatActivity() {
         PoseDetection.getClient(options)
     }
 
-    val bodyPoints = mutableMapOf<String, Double>(
-        "NOSE_X" to 0.0, "NOSE_Y" to 0.0,
-        "LEFT_EYE_INNER_X" to 0.0, "LEFT_EYE_INNER_Y" to 0.0,
-        "LEFT_EYE_X" to 0.0, "LEFT_EYE_Y" to 0.0,
-        "LEFT_EYE_OUTER_X" to 0.0, "LEFT_EYE_OUTER_Y" to 0.0,
-        "RIGHT_EYE_INNER_X" to 0.0, "RIGHT_EYE_INNER_Y" to 0.0,
-        "RIGHT_EYE_X" to 0.0, "RIGHT_EYE_Y" to 0.0,
-        "RIGHT_EYE_OUTER_X" to 0.0, "RIGHT_EYE_OUTER_Y" to 0.0,
-        "LEFT_EAR_X" to 0.0, "LEFT_EAR_Y" to 0.0,
-        "RIGHT_EAR_X" to 0.0, "RIGHT_EAR_Y" to 0.0,
-        "LEFT_MOUTH_X" to 0.0, "LEFT_MOUTH_Y" to 0.0,
-        "RIGHT_MOUTH_X" to 0.0, "RIGHT_MOUTH_Y" to 0.0,
-        "LEFT_SHOULDER_X" to 0.0, "LEFT_SHOULDER_Y" to 0.0,
-        "RIGHT_SHOULDER_X" to 0.0, "RIGHT_SHOULDER_Y" to 0.0,
-        "LEFT_ELBOW_X" to 0.0, "LEFT_ELBOW_Y" to 0.0,
-        "RIGHT_ELBOW_X" to 0.0, "RIGHT_ELBOW_Y" to 0.0,
-        "LEFT_WRIST_X" to 0.0, "LEFT_WRIST_Y" to 0.0,
-        "RIGHT_WRIST_X" to 0.0, "RIGHT_WRIST_Y" to 0.0,
-        "LEFT_PINKY_X" to 0.0, "LEFT_PINKY_Y" to 0.0,
-        "RIGHT_PINKY_X" to 0.0, "RIGHT_PINKY_Y" to 0.0,
-        "LEFT_INDEX_X" to 0.0, "LEFT_INDEX_Y" to 0.0,
-        "RIGHT_INDEX_X" to 0.0, "RIGHT_INDEX_Y" to 0.0,
-        "LEFT_THUMB_X" to 0.0, "LEFT_THUMB_Y" to 0.0,
-        "RIGHT_THUMB_X" to 0.0, "RIGHT_THUMB_Y" to 0.0,
-        "LEFT_HIP_X" to 0.0, "LEFT_HIP_Y" to 0.0,
-        "RIGHT_HIP_X" to 0.0, "RIGHT_HIP_Y" to 0.0,
-        "LEFT_KNEE_X" to 0.0, "LEFT_KNEE_Y" to 0.0,
-        "RIGHT_KNEE_X" to 0.0, "RIGHT_KNEE_Y" to 0.0,
-        "LEFT_ANKLE_X" to 0.0, "LEFT_ANKLE_Y" to 0.0,
-        "RIGHT_ANKLE_X" to 0.0, "RIGHT_ANKLE_Y" to 0.0,
-        "LEFT_HEEL_X" to 0.0, "LEFT_HEEL_Y" to 0.0,
-        "RIGHT_HEEL_X" to 0.0, "RIGHT_HEEL_Y" to 0.0,
-        "LEFT_FOOT_INDEX_X" to 0.0, "LEFT_FOOT_INDEX_Y" to 0.0,
-        "RIGHT_FOOT_INDEX_X" to 0.0, "RIGHT_FOOT_INDEX_Y" to 0.0
+    data class TargetPose(
+        val targets: List<PoseAnalysis.TargetShape>
     )
+
+    private val targetPose: TargetPose = TargetPose(
+        listOf(
+            PoseAnalysis.TargetShape(
+                PoseLandmark.LEFT_ANKLE,
+                PoseLandmark.LEFT_KNEE,
+                PoseLandmark.LEFT_HIP,
+                170.0
+            ),
+            PoseAnalysis.TargetShape(
+                PoseLandmark.LEFT_WRIST,
+                PoseLandmark.LEFT_ELBOW,
+                PoseLandmark.LEFT_SHOULDER,
+                80.0
+            )
+        )
+    )
+    var poseAnswer = false
+    private var isAutoCaptureEnabled = true
+    var yourAngle = 0.0
 
     // CameraX 이미지를 분석후, Landmark(좌표값)을 리턴해준다. 좌표값은 pose에 담겨있다.
     val onPoseDetected: (pose: Pose) -> Unit = { pose ->
+//        for (landmark in pose.allPoseLandmarks) {
+//            val landmarkType = landmark.landmarkType
+//            val position = landmark.position
+//            Log.d("PoseInfo", "Type: $landmarkType, x: ${position.x}, y: ${position.y}")
+//            // 여기서 landmarkType은 그냥 0~32 까지의 숫자로만 나옴
+//        }
+
+        // 유사도 분석으로 넘어가는 코드
+        poseAnswer = PoseAnalysis(pose,targetPose).match(pose,targetPose)
+        //유사도 true,false로 결과 출력
+        yourAngle = PoseAnalysis(pose,targetPose).yourAngle
+        checkAndCapture(poseAnswer)
 
     }
 
@@ -108,29 +116,54 @@ class cameraActivity : AppCompatActivity() {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
         }
 
+        val targetAngle = 0.0
+        targetPose.targets.forEach { target ->
+            val targetAngle = target.angle}
 
         // Set up the listeners for take photo
         viewBinding.imageCaptureButton.setOnClickListener {
-            takePhoto()
+            AlertDialog.Builder(this)
+                .setTitle("Take Photo?")
+                .setMessage("Do you want to take the photo?")
+                .setPositiveButton("Agree") { dialog, which ->
+                    takePhoto()
+                    val intent = Intent(this, ResultActivity::class.java)
+                    intent.apply {
+                        putExtra("PoseResult" , poseAnswer )
+                        putExtra("TargetAnlge",targetAngle)
+                        putExtra("YourAngle",yourAngle)
+                    }
+                    startActivity(intent)
+                }
+                .setNegativeButton("Disagree", null)
+                .show()
+        }
+
+        // Set up the listeners for gallery
+        viewBinding.galleryButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Open Gallery?")
+                .setMessage("Do you want to open the gallery?")
+                .setPositiveButton("Agree") { dialog, which ->
+                    openGalleryForImage()
+                }
+                .setNegativeButton("Disagree", null)
+                .show()
+
+//            openGalleryForImage()
 //            val intent = Intent(this, ResultActivity::class.java)
 //            intent.apply {
-//                putExtra("PoseInfo" , bodyPoints.toString())
+//                putExtra("PoseResult" , poseAnswer)
+//                putExtra("TargetAngle", targetAngle)
+//                putExtra("YourAngle",yourAngle)
 //            }
 //            startActivity(intent)
         }
-        viewBinding.galleryButton.setOnClickListener {
-            openGalleryForImage()
-
-//            val intent = Intent(this, ResultActivity::class.java)
-//            startActivity(intent)
-        }
         cameraExecutor = Executors.newSingleThreadExecutor()
-
-
-
 
     }
 
@@ -150,10 +183,11 @@ class cameraActivity : AppCompatActivity() {
         }
 
         // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(contentResolver,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues)
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
             .build()
 
         // Set up image capture listener, which is triggered after photo has
@@ -163,7 +197,7 @@ class cameraActivity : AppCompatActivity() {
             ContextCompat.getMainExecutor(this),
             object : ImageCapture.OnImageSavedCallback {
                 override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                    Log.e("ITM", "Photo capture failed: ${exc.message}", exc)
                 }
 
                 override fun onImageSaved(output: ImageCapture.OutputFileResults){
@@ -171,10 +205,13 @@ class cameraActivity : AppCompatActivity() {
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                     val savedUri = output.savedUri ?: return
                     analyzePoseFromUri(savedUri)
-                    Log.d(TAG, msg)
+                    Log.d("ITM", msg)
                 }
             }
         )
+    }
+    object ImageUriHolder {
+        var capturedImageUri: Uri? = null
     }
 
     //카메라 시작하는 함수
@@ -198,16 +235,16 @@ class cameraActivity : AppCompatActivity() {
             // Image Capture
             imageCapture = ImageCapture.Builder().build()
                 .also {
-                    cameraActivity.CameraAnalyzer(poseDetector,onPoseDetected)
+                    CameraActivity.CameraAnalyzer(poseDetector,onPoseDetected)
                 }
 
-//            val imageAnalyzer = ImageAnalysis.Builder()
-//                .build()
-//                .also{
-//                    it.setAnalyzer(cameraExecutor,
-//                        cameraActivity.CameraAnalyzer(poseDetector, onPoseDetected)
-//                    )
-//                }
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also{
+                    it.setAnalyzer(cameraExecutor,
+                        CameraActivity.CameraAnalyzer(poseDetector, onPoseDetected)
+                    )
+                }
 
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
@@ -218,7 +255,7 @@ class cameraActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture ) //imageAnalyzer 그냥 넣으면 됨
+                    this, cameraSelector, preview, imageCapture ,imageAnalyzer) //imageAnalyzer 그냥 넣으면 됨
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -237,7 +274,7 @@ class cameraActivity : AppCompatActivity() {
     // 갤러리에서 사진을 선택한 후 처리
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == GALLERY_REQUEST_CODE) {
+        if (resultCode == RESULT_OK && requestCode == GALLERY_REQUEST_CODE) {
             data?.data?.let { uri ->
                 analyzePoseFromUri(uri)
             }
@@ -249,20 +286,7 @@ class cameraActivity : AppCompatActivity() {
         val image = InputImage.fromFilePath(this, uri)
         poseDetector.process(image)
             .addOnSuccessListener { pose ->
-                for (landmark in pose.allPoseLandmarks) {
-                    val landmarkType = landmark.landmarkType
-                    val position = landmark.position
-                    Log.d("PoseInfo", "Type: $landmarkType, x: ${position.x}, y: ${position.y}")
-                    // 여기서 landmarkType은 그냥 0~32 까지의 숫자로만 나옴
-                    val bodyX = "${landmarkType}_X"
-                    val bodyY = "${landmarkType}_Y"
-
-                    bodyPoints.put(bodyX , position.x.toDouble())
-                    bodyPoints.put(bodyY, position.y.toDouble())
-
-                    Log.d("BodyPoint",bodyPoints.toString())
-                }
-                // 여기서 pose 처리
+                onPoseDetected(pose)
             }
             .addOnFailureListener { e ->
                 // 에러 처리
@@ -271,7 +295,8 @@ class cameraActivity : AppCompatActivity() {
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private class CameraAnalyzer(
@@ -311,12 +336,92 @@ class cameraActivity : AppCompatActivity() {
             if (allPermissionsGranted()) {
                 startCamera()
             } else {
-                Toast.makeText(this,
+                Toast.makeText(
+                    this,
                     "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
+                    Toast.LENGTH_SHORT
+                ).show()
                 finish()
             }
         }
+    }
+
+    // -----------------------------------------------------------------  여기서 부터 실시간 캡쳐와 푸쉬 알림 ------------------------------------------------
+
+
+    // 로컬푸시알림 구현
+
+    private val localNotificationManager: LocalNotificationManager by lazy {
+        LocalNotificationManager(this)
+    }
+    // 자동 캡쳐 및 푸시 알림 여부를 검사하는 함수
+    private fun checkAndCapture(answerScore:Boolean) {
+        // 최적의 포즈와 현재 포즈가 일치하는지 확인
+        if (answerScore && isAutoCaptureEnabled) {
+            // 자동으로 캡쳐
+            takePhoto()
+            // 푸시 알림 전송
+            getFCMToken()
+            localNotificationManager.sendGoodPositionNotification()
+            // 자동 캡쳐를 비활성화하거나 필요에 따라 플래그를 수정할 수 있음
+            isAutoCaptureEnabled = false
+        }
+    }
+
+    // 푸시 알림을 보내는 함수
+    private fun sendPushNotification(token: String) {
+        // 여기에 푸시 알림을 보내는 로직을 추가
+        // FCM 메시지 생성
+        val notification = JSONObject()
+        val notificationBody = JSONObject()
+
+        try {
+            notificationBody.put("title", "알림 제목")
+            notificationBody.put("body", "알림 내용")
+
+            notification.put("to", token)
+            notification.put("data", notificationBody)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
+        // FCM 메시지 전송
+        val request = object : JsonObjectRequest(
+            Method.POST,
+            "https://fcm.googleapis.com/fcm/send",
+            notification,
+            Response.Listener { response ->
+                // 푸시 알림 전송 성공 시의 처리
+                Log.d(TAG, "Push notification sent successfully")
+            },
+            Response.ErrorListener { error ->
+                // 푸시 알림 전송 실패 시의 처리
+                Log.e(TAG, "Error sending push notification: $error")
+            }
+        ) {
+            // FCM 서버에 요청 헤더 추가
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                headers["Authorization"] = "AAAA-VdO4-M:APA91bErPZ4l2NP7ml7iwVBW-ySuEyIY7jlSeM7l56QKOWFDR1s34O7b1jf3QMDoGnm3HrjL326HCLj45H8zuDQeUgUftvem1KbNbzn_4f2BM5LcNxvJO21QihE3cvCn5jno1fq55ARg" // Firebase Console에서 확인한 서버 키로 변경
+                return headers
+            }
+        }
+
+        // 요청 큐에 요청 추가
+        Volley.newRequestQueue(this).add(request)
+    }
+
+    private fun getFCMToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val token = task.result
+                // 토큰을 사용하여 푸시 알림을 보낼 수 있음
+                sendPushNotification(token)
+            } else {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+            }
+        })
     }
 
     companion object {
@@ -335,4 +440,3 @@ class cameraActivity : AppCompatActivity() {
             }.toTypedArray()
     }
 }
-
